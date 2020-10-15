68bbbb3e56ab331f652c06749844ba193a09c7c9
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-10-15 15:55:14 -0300

    Routeradicionado

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import './Navbar.css'
+
+
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+             <div className="navbar-caontainer">
+
+              <img src='./images/barlogo.png'/>
+                 
+                 </div>   
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
