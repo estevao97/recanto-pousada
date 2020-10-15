2325f2f19860d21d83a89a78d7e43901d700a060
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-10-15 18:42:05 -0300

    Barra

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,22 @@ import React, {useState} from 'react'
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
-        <div>
+        <>
             <nav className="navbar">
              <div className="navbar-caontainer">
 
               <img src='./images/barlogo.png'/>
-                 
+               <i className='fab fa-typo3'/>
+                 <div className='menu-icon' onClick={handleClick}>
+                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                     </div>
                  </div>   
             </nav>
-        </div>
-    )
+        </>
+    );
 }
 
 export default Navbar
