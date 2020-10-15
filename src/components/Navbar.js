484f818f7c8196b29fcc53cd1cf6f6da8ab15947
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-10-15 15:47:51 -0300

    Router adicionado

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+             <div className="navbar-caontainer">
+
+              <img src='./images/barlogo.png'/>
+                 
+                 </div>   
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
