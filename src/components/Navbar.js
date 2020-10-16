b87b61a578e780d858a09d2108f4b46e66b4e279
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-10-15 22:54:16 -0300

    ADD Barra pronta

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,76 @@
-import React, {useState} from 'react'
-
-//Menu Barra
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    return (
-        <>
-            <nav className="navbar">
-             <div className="navbar-caontainer">
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-              <img src='./images/barlogo.png'/>
-               <i className='fab fa-typo3'/>
-                 <div className='menu-icon' onClick={handleClick}>
-                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-                     </div>
-                 </div>   
-            </nav>
-        </>
-    );
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            RECANTO DE MINAS
+            
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/historia'className='nav-links'onClick={closeMobileMenu}>
+              História da Pousada
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/fotos'className='nav-links'onClick={closeMobileMenu}>
+                Fotos
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link to='/atracoes'className='nav-links'onClick={closeMobileMenu}>
+                Atrações
+              </Link>
+            </li>
+
+           
+
+            <li>
+              <Link to='/cadastro'className='nav-links-mobile'onClick={closeMobileMenu} >
+                Alugue um quarto
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>ALUGUE UM QUARTO</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
