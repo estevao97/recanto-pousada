c823de682eb83671f98cb8523a9b6171440122f8
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-10-16 21:20:40 -0300

    BUG Imagem Home

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+import Abertura from './Abertura';
+import './HeroSection.css';
+
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+
+    <h1>Um lugar onde seu conforto é prioridade</h1>
+
+   <div className='Tamanho'>
+   <Abertura/>
+   </div>
+     
+
+    </div>
+  );
+}
+
+export default HeroSection;
+
---
+++ src/components/Paginas/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Abertura from '../Abertura'
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    
+    </>
+  );
+}
+
+export default Home;
