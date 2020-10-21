f848908b4c9cab3b795481c2bd8288b262f7a462
Paulo <pauloestevao92@gmail.com>
2020-10-21 18:59:08 -0300

    Incrementado Login com bootstrap

--- src/Paginas/Login/Login.js
+++ src/Paginas/Login/Login.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 import "./Login.css";
 
 function Login() {
+  return (
+    <div className="base">
+      <div className="container">
+        <h1>Recanto de Minas</h1>
+        <Form>
+          <div className="imputs">
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Text className="text-muted">
+                Entre com seu email de cadastro
+              </Form.Text>
+            </Form.Group>
 
-    return (
-        <div className="base">
-            <div className="container">
-                <h1>Recanto de Minas</h1>
-                
-            </div>
-        </div>
-    )
+            <Form.Group controlId="senha">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
+            <Button variant="Login">Primary</Button>
+            {}
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
-
 export default Login;
