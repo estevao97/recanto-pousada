59286cf00f340cbe1e32479e7589412017abe3c7
Paulo <pauloestevao92@gmail.com>
2020-10-30 22:40:38 -0300

    MOD Cadastro

--- src/Paginas/Cadastro/Cadastro.js
+++ src/Paginas/Cadastro/Cadastro.js
@@ -3,37 +3,10 @@ import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "../../Components/Button/Button";
+import est from "./Estados";
 import "./Cadastro.css";
 
-const estados = [
-  { label: "AC", value: "Acre" },
-  { label: "AL", value: "Alagoas" },
-  { label: "AP", value: "Amapá" },
-  { label: "AM", value: "Amazonas" },
-  { label: "BA", value: "Bahia" },
-  { label: "CE", value: "Ceará" },
-  { label: "DF", value: "Distrito Federal" },
-  { label: "ES", value: "Espírito Santo" },
-  { label: "GO", value: "Goiás" },
-  { label: "MA", value: "Maranhão" },
-  { label: "MT", value: "Mato Grosso" },
-  { label: "MS", value: "Mato Grosso do Sul" },
-  { label: "MG", value: "Minas Gerais" },
-  { label: "PA", value: "Pará" },
-  { label: "PB", value: "Paraíba" },
-  { label: "PR", value: "Paraná" },
-  { label: "PE", value: "Pernambuco" },
-  { label: "PI", value: "Piauí" },
-  { label: "RJ", value: "Rio de Janeiro" },
-  { label: "RN", value: "Rio Grande do Norte" },
-  { label: "RS", value: "Rio Grande do Sul" },
-  { label: "RO", value: "Rondônia" },
-  { label: "RR", value: "Roraima" },
-  { label: "SC", value: "Santa Catarina" },
-  { label: "SP", value: "São Paulo" },
-  { label: "SE", value: "Sergipe" },
-  { label: "TO", value: "Tocantins" },
-];
+const estados = { est };
 
 const CssTextField = withStyles({
   root: {
