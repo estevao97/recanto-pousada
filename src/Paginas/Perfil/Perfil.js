82ec4a0772dd38acf123969e0ad62a174b1d4f97
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-11-01 14:17:00 -0300

    ADD fotos + BUG Perfil

--- src/Paginas/Perfil/Perfil.js
+++ src/Paginas/Perfil/Perfil.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Perfil.css";
+import imgPerfil from "../../images/perfil.svg";
 
 function Perfil() {
   return (
     <div>
-      <div className="Base">
-        <div className="Dados"></div>
-        Quarto reservado em: 20/04/2018
+      <div className="Dados">
+        <div className="foto">
+          <h1>Bryan Azevedo</h1>
+          <img className="imagem" src={imgPerfil} alt="" />
+        </div>
       </div>
+      <div className="Reservas">Quarto de casal alugado em: 02/03/2019</div>
+      <div className="Base"></div>
     </div>
   );
 }
