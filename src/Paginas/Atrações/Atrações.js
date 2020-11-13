a57d7bc24a44f00dd712c3d379e9f7355bb0bfd7
Yasmim Ribeiro <yasmimcmr13@gmail.com>
2020-11-13 00:07:00 -0300

    ADD new atrações
    nova pagina de atrações

--- src/Paginas/Atrações/Atrações.js
+++ src/Paginas/Atrações/Atrações.js
@@ -1,106 +1,109 @@
 import React from "react";
 import "./Atrações.css";
+import Footer from "../../Components/Footer/Footer";
+import CardAtrações from "../../Components/CardAtrações/CardAtrações";
 
 function Atrações() {
   return (
-    <div className="baseatracoes">
-      <div className="container-atracoes">
-        <h1>Atrações</h1>
+    <div className="baseAtrações">
+      <h1>Atrações</h1>
+      
+     
+        
+        <div className="coluna1">
+          <CardAtrações
+            title="Tirolesa"
+            title2="tirolesa"
+            image="https://images.pexels.com/photos/2041759/pexels-photo-2041759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            apresentacao="Uma otima aventura para quem gosta de sentir um frio na barriga,e adora uma adrenalina passando pelo sangue"
+            detalhamento="Nessa atividade de aventura, o praticante é preso por mosquetões a uma cadeirinha de alpinismo, e se 
+         desloca com roldanas de um ponto a outro por um cabo aéreo posicionado horizontalmente. O cabo, é claro, fica nas alturas, 
+         normalmente conectado a árvores, ao ar livre. Isso significa que o praticante tem a sensação de “voar” sobre o local. 
+         Muitas tirolesas estão acima de florestas, lagos e outras lindas paisagens, o que torna a experiência mais prazerosa.s
 
-        <div className="thumb">
-          <div className="coluna1">
-            <div className="cardatracao">
-              <h3>Bung Jump</h3>
-              <img
-                className="imagem1"
-                src="https://images.pexels.com/photos/2041759/pexels-photo-2041759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="aqui tem uma tirolesa"
-              />
-              <div className=" bungjump">
-                Temos o bung jump<br></br> Uma ótima atração para quem gosta de
-                adrenalina!<br></br> Valor por pessoa: R$30,00.
-              </div>
-            </div>
+         "
+          />
+          <CardAtrações 
+            title="Bung Jump"
+            title2="Bung Jump"
+            image="https://images.pexels.com/photos/1486036/pexels-photo-1486036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            apresentacao="Uma adrenalina e tanto, para quem tem coragem! E gosta de se aventurar nas alturas"
+            detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
+         fazendo-o cair mais uma ou duas vezes. Assim, a prática em si dura um pouco mais de um minuto, mas o frio na barriga começa muito antes.
+        Afinal, todo o processo de preparo do material e a escolha do melhor lugar para saltar já colocam a sua expectativa lá em cima, 
+         fazendo com que você produza ainda mais adrenalina para o grande momento.
+         Viu só como pular de bungee jump pode ser uma aventura e tanto? Se você é uma das pessoas 
+         que não deixa passar qualquer oportunidade de aventura, não deixe de experimentar essa atividade 
+         para estimular o seu contato com a natureza e sentir na pele toda a liberdade que ela carrega.
 
-            <div className="cardatracao">
-              <h3>Kaiak</h3>
-              <img
-                className="imagem2"
-                src="https://gooutside-static-cdn.akamaized.net/wp-content/uploads/sites/3/2018/09/oru-haven-tandem-kayak-2.jpg"
-                alt="aqui tem um kaiak"
-              />
-              <div className="texto2">
-                O kaik é uma atração para quem adora sentir aquele frio na
-                barriga, é muito desafiante e restaurador de energias
-                hdajsdjshdjsahdasjd hsadghsdsadg
-                <br></br>
-                Valor por pessoa: R$15,00.
-              </div>
-            </div>
-            <div className="cardatracao">
-              <h3>Asa Delta</h3>
-              <img
-                className="imagem3"
-                src="https://images.pexels.com/photos/53012/glider-hang-glider-pilot-flying-53012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="aqui tem uma asa delta"
-              />
-              <div className="texto3">
-                A asa delta é uma ótima atração para quem quer se sentir nas
-                alturas!<br></br> Venha conferir esta novidade!! <br></br> Valor
-                por pessoa: R$100,00;
-              </div>
-            </div>
-          </div>
-          <div className="coluna2">
-            <div className="cardatracao">
-              <h3>Pescaria</h3>
-              <img
-                className="imagem4"
-                src="https://images.pexels.com/photos/1143926/pexels-photo-1143926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="aqui tem uma imagem de pescaria"
-              />
-              <div className="texto4">
-                A pescaria é uma boa opção para quem gosta de ficar em paz e
-                relaxar<br></br>Valor por pessoa: Grátis,apenas paga pelo valor
-                do pescado.
-              </div>
-            </div>
+         "
+          />
+          <CardAtrações
+            className="card2"
+            title="Stand up Paddle "
+            title2="Stand up Paddle"
+            image="https://images.pexels.com/photos/1751550/pexels-photo-1751550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            apresentacao="Uma adrenalina e tanto para quem tem coragem!"
+            detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
+         fazendo-o cair mais uma ou duas vezes. Assim, a prática em si dura um pouco mais de um minuto, mas o frio na barriga começa muito antes.
+        Afinal, todo o processo de preparo do material e a escolha do melhor lugar para saltar já colocam a sua expectativa lá em cima, 
+         fazendo com que você produza ainda mais adrenalina para o grande momento.
+         Viu só como pular de bungee jump pode ser uma aventura e tanto? Se você é uma das pessoas 
+         que não deixa passar qualquer oportunidade de aventura, não deixe de experimentar essa atividade 
+         para estimular o seu contato com a natureza e sentir na pele toda a liberdade que ela carrega.
 
-            <div className="cardatracao">
-              <h3>Tirolesa </h3>
-              <img
-                className="imagem5"
-                src="https://cdnstatic8.com/acrocabo.com.br/wp-content/uploads/2019/10/1_TIROLESA_Ion-David-4224.jpg"
-                alt="Tirolesa"
-              />
-              <div className="texto5">
-                O misto de medo antes de pular e a liberdade de se sentir quase
-                voando! Essa é a sensação que quem se <br></br>aventura em nossa
-                tirolesa costuma contar.
-                <br></br> Valor por pessoa:R$15,00
-              </div>
-            </div>
-
-            <div className="cardatracao">
-              <h3>Passeio à Cavalo</h3>
-              <img
-                className="imagem6"
-                src="https://images.pexels.com/photos/1364073/pexels-photo-1364073.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="aqui tem uma imagem de stand up peddle"
-              />
-              <div className="texto6">
-                Temos nosso passeio a cavalo, todos costumam adorar!<br></br>
-                Venha conferir!<br></br>
-                Valor por pessoa: Grátis!
-              </div>
-            </div>
-          </div>
+         "
+          />
         </div>
-        <footer class="kb-footer">
-          <div class="kb-footer-wrapper templete centered"></div>
-        </footer>
+
+        <div className="coluna2">
+          <CardAtrações
+            title="Caiaque"
+            title2="caiaque"
+            image="https://gooutside-static-cdn.akamaized.net/wp-content/uploads/sites/3/2018/09/oru-haven-tandem-kayak-2.jpg"
+            apresentacao="O caiaque é uma ótima forma de se desafiar nas aguas agitadas da nossa pousada"
+            detalhamento="O caiaque é uma pequena embarcação a remos utilizada para lazer, 
+         transporte e competições. Na vertente desportiva compreende várias modalidades como velocidade, 
+         slalom, adaptada, descida, maratona, oceânica, onda, pólo, rafting e rodeio
+        Esta embarcação começou a ficar famosa a partir da década de 1970, criada na Groenlândia
+
+         "
+          />
+          <CardAtrações
+            title="Asa Delta"
+            title2="Asa Delta"
+            image="https://images.pexels.com/photos/53012/glider-hang-glider-pilot-flying-53012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            apresentacao="A Asa delta é muito deasfiadora, quem gosta de se sentir nas alturas vai amar nossa atração!"
+            detalhamento="A asa delta é uma das modalidades mais famosas de esportes radicais praticados no ar que existem, 
+         principalmente em cidades com montanhas ou terrenos propícios para os voos.
+         Assim, para pode voar de asa delta não é necessário o uso de aviões para o transporte dos praticantes até o céu.
+         Uma vez que o salto é realizado, os atletas podem voar de forma indefinida, desde que haja raios solares suficientes 
+         para aquecer o solo e, por consequência, o ar, que vai fazer o equipamento planar.
+         Dessa forma, é possível se manter estável e deslizando pelo céu por um longo período, se for desejo de quem está no 
+         controle da asa delta ou enquanto o Sol permitir.
+
+         "
+          />
+          <CardAtrações
+            title="Pescaria"
+            title2="Pescaria"
+            image="https://images.pexels.com/photos/1143926/pexels-photo-1143926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            apresentacao="A pescaria é nossa atração para aquelas pessoas que pretendem relaxar,venham conferir!"
+            detalhamento="Saber aproveitar tudo o que a atividade envolve, também faz bem para quem pratica a mesma, 
+            como curtir o sol, o verde, o vento, o som dos pássaros, das águas e aprender um pouco mais sobre os tipos de peixes e a 
+            respeito da natureza. Além disso, é importante destacar que, quem pesca, exercita a paciência e autoconfiança. A paciência, 
+            porque é preciso aguardar até o momento em que o peixe é fisgado pela isca. E, a autoconfiança, porque o indivíduo irá aguçar a 
+            vontade de superar cada vez mais os próprios limites.
+            O exercício da pesca, também traz ao praticante, a oportunidade de conhecer pessoas, fazer novos amigos e até companheiros de pesca. 
+            Isso faz com que o indivíduo possa interagir, contar histórias e dividir experiências.
+         "
+          />
+        </div>
+
       </div>
-    </div>
+      
+   
+    
   );
 }
 
