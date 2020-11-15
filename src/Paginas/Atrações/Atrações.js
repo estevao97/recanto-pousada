24339f6e0df25307f58812e37acdf3280f79f985
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-11-15 17:34:54 -0300

    ADD atracoes terminada

--- src/Paginas/Atrações/Atrações.js
+++ src/Paginas/Atrações/Atrações.js
@@ -2,32 +2,32 @@ import React from 'react';
 import './Atrações.css';
 import Footer from '../../Components/Footer/Footer';
 import CardAtrações from '../../Components/CardAtrações/CardAtrações';
-
 function Atrações() {
   return (
-    <div className="baseAtrações">
-      <h1>Atrações</h1>
+    <>
+      <div className="baseAtrações">
+        <h1>Atrações</h1>
 
-      <div className="coluna1">
-        <div className="cartao1">
-          <CardAtrações
-            title="Tirolesa"
-            title2="tirolesa"
-            image="https://images.pexels.com/photos/2041759/pexels-photo-2041759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            apresentacao="Uma otima aventura para quem gosta de sentir um frio na barriga,e adora uma adrenalina passando pelo sangue"
-            detalhamento="Nessa atividade de aventura, o praticante é preso por mosquetões a uma cadeirinha de alpinismo, e se 
+        <div className="coluna1">
+          <div className="cartao1">
+            <CardAtrações
+              title="Tirolesa"
+              title2="tirolesa"
+              image="https://images.pexels.com/photos/2041759/pexels-photo-2041759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              apresentacao="Uma otima aventura para quem gosta de sentir um frio na barriga,e adora uma adrenalina passando pelo sangue"
+              detalhamento="Nessa atividade de aventura, o praticante é preso por mosquetões a uma cadeirinha de alpinismo, e se 
          desloca com roldanas de um ponto a outro por um cabo aéreo posicionado horizontalmente. O cabo, é claro, fica nas alturas, 
          normalmente conectado a árvores, ao ar livre. Isso significa que o praticante tem a sensação de “voar” sobre o local. 
          Muitas tirolesas estão acima de florestas, lagos e outras lindas paisagens, o que torna a experiência mais prazerosa.s
 
          "
-          />
-          <CardAtrações
-            title="Bung Jump"
-            title2="Bung Jump"
-            image="https://images.pexels.com/photos/1486036/pexels-photo-1486036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            apresentacao="Uma adrenalina e tanto, para quem tem coragem! E gosta de se aventurar nas alturas"
-            detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
+            />
+            <CardAtrações
+              title="Bung Jump"
+              title2="Bung Jump"
+              image="https://images.pexels.com/photos/1486036/pexels-photo-1486036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              apresentacao="Uma adrenalina e tanto, para quem tem coragem! E gosta de se aventurar nas alturas"
+              detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
          fazendo-o cair mais uma ou duas vezes. Assim, a prática em si dura um pouco mais de um minuto, mas o frio na barriga começa muito antes.
         Afinal, todo o processo de preparo do material e a escolha do melhor lugar para saltar já colocam a sua expectativa lá em cima, 
          fazendo com que você produza ainda mais adrenalina para o grande momento.
@@ -36,14 +36,14 @@ function Atrações() {
          para estimular o seu contato com a natureza e sentir na pele toda a liberdade que ela carrega.
 
          "
-          />
-          <CardAtrações
-            className="card2"
-            title="Stand up Paddle "
-            title2="Stand up Paddle"
-            image="https://images.pexels.com/photos/1751550/pexels-photo-1751550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            apresentacao="Uma adrenalina e tanto para quem tem coragem!"
-            detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
+            />
+            <CardAtrações
+              className="card2"
+              title="Stand up Paddle "
+              title2="Stand up Paddle"
+              image="https://images.pexels.com/photos/1751550/pexels-photo-1751550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              apresentacao="Uma adrenalina e tanto para quem tem coragem!"
+              detalhamento="Em seguida, quando o elástico atinge sua potência máxima, ele puxa o praticante para cima, 
          fazendo-o cair mais uma ou duas vezes. Assim, a prática em si dura um pouco mais de um minuto, mas o frio na barriga começa muito antes.
         Afinal, todo o processo de preparo do material e a escolha do melhor lugar para saltar já colocam a sua expectativa lá em cima, 
          fazendo com que você produza ainda mais adrenalina para o grande momento.
@@ -52,30 +52,30 @@ function Atrações() {
          para estimular o seu contato com a natureza e sentir na pele toda a liberdade que ela carrega.
 
          "
-          />
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="coluna2">
-        <div className="cartao2">
-          <CardAtrações
-            title="Caiaque"
-            title2="caiaque"
-            image="https://gooutside-static-cdn.akamaized.net/wp-content/uploads/sites/3/2018/09/oru-haven-tandem-kayak-2.jpg"
-            apresentacao="O caiaque é uma ótima forma de se desafiar nas aguas agitadas da nossa pousada"
-            detalhamento="O caiaque é uma pequena embarcação a remos utilizada para lazer, 
+        <div className="coluna2">
+          <div className="cartao2">
+            <CardAtrações
+              title="Caiaque"
+              title2="caiaque"
+              image="https://gooutside-static-cdn.akamaized.net/wp-content/uploads/sites/3/2018/09/oru-haven-tandem-kayak-2.jpg"
+              apresentacao="O caiaque é uma ótima forma de se desafiar nas aguas agitadas da nossa pousada"
+              detalhamento="O caiaque é uma pequena embarcação a remos utilizada para lazer, 
          transporte e competições. Na vertente desportiva compreende várias modalidades como velocidade, 
          slalom, adaptada, descida, maratona, oceânica, onda, pólo, rafting e rodeio
         Esta embarcação começou a ficar famosa a partir da década de 1970, criada na Groenlândia
 
          "
-          />
-          <CardAtrações
-            title="Asa Delta"
-            title2="Asa Delta"
-            image="https://images.pexels.com/photos/53012/glider-hang-glider-pilot-flying-53012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            apresentacao="A Asa delta é muito deasfiadora, quem gosta de se sentir nas alturas vai amar nossa atração!"
-            detalhamento="A asa delta é uma das modalidades mais famosas de esportes radicais praticados no ar que existem, 
+            />
+            <CardAtrações
+              title="Asa Delta"
+              title2="Asa Delta"
+              image="https://images.pexels.com/photos/53012/glider-hang-glider-pilot-flying-53012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              apresentacao="A Asa delta é muito deasfiadora, quem gosta de se sentir nas alturas vai amar nossa atração!"
+              detalhamento="A asa delta é uma das modalidades mais famosas de esportes radicais praticados no ar que existem, 
          principalmente em cidades com montanhas ou terrenos propícios para os voos.
          Assim, para pode voar de asa delta não é necessário o uso de aviões para o transporte dos praticantes até o céu.
          Uma vez que o salto é realizado, os atletas podem voar de forma indefinida, desde que haja raios solares suficientes 
@@ -84,13 +84,13 @@ function Atrações() {
          controle da asa delta ou enquanto o Sol permitir.
 
          "
-          />
-          <CardAtrações
-            title="Pescaria"
-            title2="Pescaria"
-            image="https://images.pexels.com/photos/1143926/pexels-photo-1143926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            apresentacao="A pescaria é nossa atração para aquelas pessoas que pretendem relaxar,venham conferir!"
-            detalhamento="Saber aproveitar tudo o que a atividade envolve, também faz bem para quem pratica a mesma, 
+            />
+            <CardAtrações
+              title="Pescaria"
+              title2="Pescaria"
+              image="https://images.pexels.com/photos/1143926/pexels-photo-1143926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              apresentacao="A pescaria é nossa atração para aquelas pessoas que pretendem relaxar,venham conferir!"
+              detalhamento="Saber aproveitar tudo o que a atividade envolve, também faz bem para quem pratica a mesma, 
             como curtir o sol, o verde, o vento, o som dos pássaros, das águas e aprender um pouco mais sobre os tipos de peixes e a 
             respeito da natureza. Além disso, é importante destacar que, quem pesca, exercita a paciência e autoconfiança. A paciência, 
             porque é preciso aguardar até o momento em que o peixe é fisgado pela isca. E, a autoconfiança, porque o indivíduo irá aguçar a 
@@ -98,10 +98,13 @@ function Atrações() {
             O exercício da pesca, também traz ao praticante, a oportunidade de conhecer pessoas, fazer novos amigos e até companheiros de pesca. 
             Isso faz com que o indivíduo possa interagir, contar histórias e dividir experiências.
          "
-          />
+            />
+          </div>
         </div>
       </div>
-    </div>
+
+      <Footer />
+    </>
   );
 }
 export default Atrações;
